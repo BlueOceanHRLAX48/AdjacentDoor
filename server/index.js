93425e6138ed27f611bc917468c2a924e5e97444
Ernest Zhang <ernestzhang97@gmail.com>
2022-01-28 15:56:04 -0800

    inital express server setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const router = require('./router')
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log('listening on ', port)
+})
+
