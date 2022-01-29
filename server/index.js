a9f794eebbd1f2496d07866305be2a96a6aca7e8
Ernest Zhang <ernestzhang97@gmail.com>
2022-01-28 16:18:42 -0800

    rebase

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const router = require('./router')
+const router = require('./router/route')
 
 const port = process.env.PORT || 3000;
 
