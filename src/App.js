640aabbd8b9573a54cbca76fadbd848bddbe8bd8
milesmaroney <miles@milesmaroney.com>
2022-01-28 18:07:24 -0800

    Implemented base design for leaderboard full page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Leaderboard from './Leaderboard';
 import Login from './Login';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
       </Routes>
     </BrowserRouter>
   );
