b559958e86104074803b28912bb4ae3a049b813e
milesmaroney <miles@milesmaroney.com>
2022-01-28 18:07:24 -0800

    Implemented base design for leaderboard full page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Leaderboard from './Leaderboard';
 import Login from './Login';
 import MyProfile from './MyProfile';
 import Groups from './Groups/Groups';
@@ -26,7 +27,7 @@ function App() {
       { id: 1, name: 'Group Name', joinStatus: 'joined' },
       { id: 2, name: 'Another Group', joinStatus: 'pending' },
     ],
-    admin: true
+    admin: true,
   });
   const [currentLocation, setCurrentLocation] = React.useState({});
 
@@ -48,15 +49,23 @@ function App() {
     );
   }, 300000);
 
-
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/my-profile' element={<MyProfile />} />
-        <Route path='/groups' element={<Groups user={user} currentLocation={currentLocation} setUser={setUser}/>} />
+        <Route
+          path='/groups'
+          element={
+            <Groups
+              user={user}
+              currentLocation={currentLocation}
+              setUser={setUser}
+            />
+          }
+        />
+        <Route path='/leaderboard' element={<Leaderboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/LeaderboardUser.js
@@ -0,0 +1,40 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+
+function LeaderboardUser(props) {
+  function getPlaceColor(place) {
+    if (place === 1) {
+      return '#FFD700';
+    }
+    if (place === 2) {
+      return '#C0C0C0';
+    }
+    if (place === 3) {
+      return '#CD7F32';
+    }
+    return '#F8F7FF';
+  }
+
+  return (
+    <div className='flex w-full items-center px-4 py-2 gap-4 hover:bg-antiqueWhite transition-all duration-150'>
+      <div className='flex w-[3%] font-bold'>{props.place}</div>
+      <Avatar
+        sx={{
+          width: 35,
+          height: 35,
+          fontSize: 16,
+          color: 'black',
+          backgroundColor: getPlaceColor(props.place),
+          border: '1px #B8B8FF solid',
+        }}
+      >
+        {props.user.name.slice(0, 1)}
+        {props.user.name.split(' ')[1].slice(0, 1)}
+      </Avatar>
+      {props.user.name}
+      <div className='ml-auto text-xs text-gray-400'>{props.user.location}</div>
+    </div>
+  );
+}
+
+export default LeaderboardUser;
