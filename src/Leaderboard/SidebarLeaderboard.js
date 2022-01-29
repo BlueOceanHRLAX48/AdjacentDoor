33d48f10758ffafcd513a900805d092c588d9979
milesmaroney <miles@milesmaroney.com>
2022-01-29 12:41:17 -0800

    Refactored leaderboard organization and implemented sidebar leaderboard to right bar

---
+++ src/Leaderboard/SidebarLeaderboard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LeaderboardUser from './LeaderboardUser';
+import SidebarUser from './SidebarUser';
+
+function SidebarLeaderboard() {
+  const sampleData = [
+    { name: 'John Smith', contribution: 45, location: 'Los Angeles, CA' },
+    { name: 'Mike Lemon', contribution: 22, location: 'San Diego, CA' },
+    { name: 'Elton Lemon', contribution: 169, location: 'Seattle, WA' },
+    { name: 'Quinton Maki', contribution: 200, location: 'Las Vegas, NV' },
+    { name: 'Ben Bernardy', contribution: 11, location: 'San Francisco, CA' },
+  ];
+
+  const leaderboardElements = sampleData
+    .slice(0, 5)
+    .map((user, i) => <SidebarUser user={user} place={i + 1} key={i} />);
+  return (
+    <div className='flex flex-col justify-center ml-4 mt-4 py-2 rounded-lg border border-primary'>
+      <div className='font-bold text-center py-2'>Top Global Contibutors</div>
+      {leaderboardElements}
+      <div className='w-full flex justify-center py-4'>
+        <Link to='/leaderboard'>
+          <button className='hover:bg-primary rounded-full px-8 py-1 font-semibold ring-primary ring-1 transition-all duration-150 hover:text-white'>
+            VIEW FULL LIST
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SidebarLeaderboard;
