577cb2b2e3e9e51a663a8a548ee471f75db6d419
davidsung <dks99455@gmail.com>
2022-01-29 12:28:19 -0800

    cleaned up group code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+<<<<<<< HEAD
 import MyProfile from './MyProfile';
+=======
+import Groups from './Groups/Groups';
+>>>>>>> dc7d299 (cleaned up group code)
 
 function App() {
   return (
@@ -11,6 +15,11 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/my-profile' element={<MyProfile />} />
       </Routes>
+<<<<<<< HEAD
+=======
+
+      <Groups />{/* Temporary locaiton to test Groups */}
+>>>>>>> dc7d299 (cleaned up group code)
     </BrowserRouter>
   );
 }
