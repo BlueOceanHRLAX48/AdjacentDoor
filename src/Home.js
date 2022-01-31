c4bf85b963f4a045aa1b46809f0cc4977f990fd0
emilyhu08 <emilyhu08@gmail.com>
2022-01-31 14:30:42 -0800

    add nav on bottom on mobile

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import Feed from './components/Feed';
 import LeftBar from './LeftBar';
 import RightBar from './RightBar';
 import TopNav from './TopNav';
+import { MdGroups, MdHealthAndSafety, MdHome } from 'react-icons/md';
 
 function Home() {
   const [leftClass, setLeftClass] = useState(false);
@@ -26,8 +27,11 @@ function Home() {
           <div className='h-screen overflow-y-scroll hide-scroll-bar'>
             <Feed />
           </div>
-          <div className='items-center justify-between bg-white sm:hidden'>
-            <MdMenu className='absolute inset-x-0 bottom-0 h-16' size='30' onClick={showMenu} />
+          <div className='fixed bottom-0 flex items-center justify-between w-screen px-4 py-2 bg-white sm:hidden'>
+            <MdHome className='' size='30' onClick={showMenu} />
+            <MdHealthAndSafety className='' size='30' onClick={showMenu} />
+            <MdGroups className='' size='30' onClick={showMenu} />
+            <MdMenu className='' size='30' onClick={showMenu} />
           </div>
           <div className='hidden sm:flex'>
             <RightBar />
