0c0672ca6a03b2571b845106e0724a1cb5d15458
“Cynthia <xin2604@gmail.com>
2022-02-02 13:43:09 -0800

    update schema and create indexes

--- database/schema.sql
+++ database/schema.sql
@@ -26,7 +26,8 @@ CREATE TABLE IF NOT EXISTS default_groups(
   city text NOT NULL,
   "state" text NOT NULL,
   zip text NOT NULL,
-  photo text NOT NULL
+  photo text NOT NULL,
+  "description" text
 );
 
 DROP TABLE IF EXISTS user_groups CASCADE;
@@ -40,7 +41,8 @@ CREATE TABLE IF NOT EXISTS user_groups(
   latitude FLOAT NOT NULL,
   longitude FLOAT NOT NULL,
   privacy boolean DEFAULT false,
-  photo text NOT NULL
+  photo text NOT NULL,
+  "description" text
 );
 
 DROP TABLE IF EXISTS groups_rating CASCADE;
@@ -124,7 +126,7 @@ SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('default_groups', 'id')), (SELECT (
 SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('user_groups', 'id')), (SELECT (MAX("id") + 1) FROM "user_groups"), FALSE);
 
 INSERT INTO user_account(firstName, lastName, username, email, network_id, city, state, zip, profile_img, privacy, contribution, default_groupID)
-VALUES ('ernest','zhang','ez', '12345@gmail.com','1124asfas', 'Ada', 'Michigan', '49301', '1234.com', DEFAULT, DEFAULT, 1);
+VALUES ('ernest','zhang','ez', '12345@gmail.com', '1124asfas' , 'Ada', 'Michigan', '49301', '1234.com', DEFAULT, DEFAULT, 1);
 
 INSERT INTO user_account(firstName, lastName, username,  email, network_id, city, state, zip, profile_img, privacy, contribution, default_groupID)
 VALUES ('ernst','zheng', 'ez123','23456@gmail.com', '12l5kjasf','Canton', 'Michigan', '02021', '1sf.com', DEFAULT, DEFAULT, 2);
@@ -132,14 +134,14 @@ VALUES ('ernst','zheng', 'ez123','23456@gmail.com', '12l5kjasf','Canton', 'Michi
 INSERT INTO user_account(firstName, lastName, username,  email, network_id, city, state, zip, profile_img, privacy, contribution, default_groupID)
 VALUES ('arnest','zhung', 'ez456','afas@gmail.com', '09afaspoi','Detroit', 'Michigan', '48708', 'asd.com', DEFAULT, DEFAULT, 3);
 
-INSERT INTO user_groups(name, admin_id, city, state, zip, latitude, longitude,privacy, photo,)
-VALUES ('the group', '1124asfas', 'city', 'state', '5678', 123, -456, DEFAULT, '1234.com');
+INSERT INTO user_groups(name, admin_id, city, state, zip, latitude, longitude,privacy, photo, description)
+VALUES ('the group', '1124asfas', 'city', 'state', '5678', 123, -456, DEFAULT, '1234.com', 'a group description');
 
-INSERT INTO user_groups(name, admin_id, city, state, zip, latitude, longitude,privacy, photo, )
-VALUES ('the second group', '1124asfas', 'city', 'state', '5678', 123, -456, DEFAULT, '1234.com');
+INSERT INTO user_groups(name, admin_id, city, state, zip, latitude, longitude,privacy, photo, description)
+VALUES ('the second group', '1124asfas', 'city', 'state', '5678', 123, -456, DEFAULT, '1234.com', 'group description');
 
 INSERT INTO default_groups(name, city, state, zip, photo)
-VALUES ('Ada', 'Ada', 'Michigan', '49301', 'photo.com';
+VALUES ('Ada', 'Ada', 'Michigan', '49301', 'photo.com');
 
 INSERT INTO default_groups(name, city, state, zip, photo)
 VALUES ('Canton', 'Canton', 'Michigan', '02021', 'photo.com');
@@ -154,11 +156,20 @@ INSERT INTO user_group_list(user_id, network_id, user_group_id, accepted) VALUES
 INSERT INTO posts(group_id, user_group_id, user_id, body, time, report, tag, privacy, latitude, longitude)
 VALUES(1, null, 1, 'asfasfasf', default, default,  'Sell', default, 123, -456.5);
 
+INSERT INTO post_imgs(post_id, image_url)
+VALUES(1, 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg');
+
 INSERT INTO replies(user_id, username, post_id, reply, report,  time, latitude, longitude)
 VALUES(2, 'username', 1, 'hihi', default, default,  567, 123.5);
 
 INSERT INTO replies(user_id,  username, post_id, reply, report,  time, latitude, longitude)
-VALUES(3, 'gggggg', 1, 'byebye', default, default,  456, 89.5);
+VALUES(2, 'gggggg', 1, 'byebye', default, default,  456, 89.5);
+
+INSERT INTO groups_rating (group_id, network_id, "safety", friendliness)
+VALUES (1, '1124asfas', 3, 4);
+
+INSERT INTO groups_rating (default_id, network_id, "safety", friendliness)
+VALUES (1, '1124asfas', 5, 5);
 
 CREATE INDEX user_user_id on user_account(user_id);
 CREATE INDEX user_network_id on user_account(network_id);
@@ -168,3 +179,5 @@ CREATE INDEX posts_id on posts(post_id);
 CREATE INDEX replies_id on replies(id);
 CREATE INDEX post_img_id on post_imgs(id);
 CREATE INDEX group_rating_id on groups_rating(id);
+CREATE INDEX user_group_list_accepted on user_group_list(accepted);
+CREATE INDEX posts_deleted on posts(deleted);
