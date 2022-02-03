8680f54731eca9ad79738b3e50d96dfab6ee6b6a
emilyhu08 <emilyhu08@gmail.com>
2022-02-03 09:19:30 -0800

    add profile

--- src/MyProfile.js
+++ src/MyProfile.js
@@ -1,7 +1,8 @@
 import { Avatar } from '@mui/material';
 import LeftBar from './LeftBar/index';
 
-function MyProfile() {
+function MyProfile({ user }) {
+  console.log(user);
   return (
     <div className='flex dark:bg-gray-900 dark:text-white'>
       <div className='hidden sm:flex'>
@@ -11,14 +12,14 @@ function MyProfile() {
       <div className='w-[600px] min-h-screen p-4 flex items-center justify-center'>
         <Avatar
           alt='Remy Sharp'
-          src='/static/images/avatar/1.jpg'
+          src={user.profile_img || '/static/images/avatar/1.jpg'}
           sx={{ width: 100, height: 100 }}
           className='mr-8'
         />
         <div className='text-base'>
-          <div className='font-semibold'>Full Name</div>
-          <div className='font-light'>Email</div>
-          <div className='font-light'>Address</div>
+          <div className='font-semibold'>{user.username}</div>
+          <div className='font-light'>{user.email}</div>
+          <div className='font-light'>{user.city + ', ' + user.state + ', ' + user.zip}</div>
         </div>
       </div>
     </div>
