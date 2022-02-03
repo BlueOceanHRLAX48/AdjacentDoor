f81c3a9b9bf2dbd16b92677559eaaacab3677238
milesmaroney <miles@milesmaroney.com>
2022-02-03 10:46:54 -0800

    fixed footer leftbar not coming back

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,35 @@
-import { MdGroups, MdHealthAndSafety, MdHome, MdMenu, MdOutlineAddCircle } from 'react-icons/md';
+import React from 'react';
+import {
+  MdGroups,
+  MdHealthAndSafety,
+  MdHome,
+  MdMenu,
+  MdOutlineAddCircle,
+} from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
 
 function Footer() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+
   const openLeftBar = () => {
     document.getElementById('left-bar').style.display = 'flex';
   };
   const closeLeftBar = () => {
     document.getElementById('left-bar').style.display = 'none';
   };
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (width > 630) {
+    document.getElementById('left-bar').style.display = 'flex';
+  }
 
   return (
     <div className='fixed bottom-0 z-10 flex items-center justify-between w-screen px-4 py-2 bg-white border sm:hidden'>
