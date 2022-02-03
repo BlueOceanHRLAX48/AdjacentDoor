be72732e4e8be605904cb0aa067bbce418fc05b0
Ben Pintel <bpintel@gmail.com>
2022-02-03 12:30:40 -0800

    photo upload correctly setting state of photo in MakeGroup

--- src/Groups/UploadPhoto.js
+++ src/Groups/UploadPhoto.js
@@ -8,32 +8,14 @@ const UploadPhoto = (props) => {
 
   const storeImages = (imageInput) => {
     const formData = new FormData();
-    formData.append('file', imageInput);
-    formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
-    axios.post(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY}/image/upload`, formData)
-    .then((response) => {
-      console.log(response.data.secure_url)
-      setPhoto(response.data.secure_url);
-    })
-    .catch((err) => {console.log(err)})
-
-  }
-
-  const [photo, setPhoto] = useState('');
-
-  const storeImages = (imageInput) => {
-    console.log(imageInput)
-    const formData = new FormData();
     formData.append('file', imageInput[0]);
     formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
     axios.post(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY}/image/upload`, formData)
     .then((response) => {
-      
-      console.log(response.data.secure_url);
       setPhoto(response.data.secure_url);
     })
     .catch((err) => {console.log(err)})
-    
+
   }
 
   return (
@@ -47,7 +29,7 @@ const UploadPhoto = (props) => {
       }} />
       <Button
         onClick={() => {
-
+          props.setPhoto(photo);
         }}
       >
         UPLOAD PHOTO
