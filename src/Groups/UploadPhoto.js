0b9d661983c2c8821eab2574a13c26b5b1a4db38
davidsung <dks99455@gmail.com>
2022-02-03 11:09:24 -0800

    partial implementation of uploadphotos

--- src/Groups/UploadPhoto.js
+++ src/Groups/UploadPhoto.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { Button, Typography } from '@mui/material';
+import axios from 'axios';
 
 const UploadPhoto = ({props}) => {
+
+  const [photo, setPhoto] = useState('');
+
+  const storeImages = (imageInput) => {
+    const formData = new FormData();
+    formData.append('file', imageInput);
+    formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
+    axios.post(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY}/image/upload`, formData)
+    .then((response) => {
+      console.log(response.data.secure_url)
+      setPhoto(response.data.secure_url);
+    })
+    .catch((err) => {console.log(err)})
+
+  }
 
   return (
     <div>
@@ -9,11 +25,14 @@ const UploadPhoto = ({props}) => {
         Would you like to upload a photo?
       </Typography>
       {/* PHOTO FUNCTIONALITY */}
+      <input type="file" onChange={(e) => {
+        storeImages(e.target.file);
+      }} />
       <Button
         onClick={() => {
 
         }}
-        >
+      >
         UPLOAD PHOTO
       </Button>
       <Button
