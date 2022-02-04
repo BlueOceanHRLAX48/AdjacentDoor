8961149e8651a2467387e192e3bd45daeb1cedf8
Weiran Cheng <chengwr1128@gmail.com>
2022-02-03 21:33:06 -0800

    modify post info

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,7 @@ function Post({
   report,
   getPosts,
   post,
+  group,
 }) {
   const [liked, setLiked] = React.useState(() =>
     JSON.parse(localStorage.getItem(`adLiked${user.network_id}${postId}`))
@@ -58,7 +59,7 @@ function Post({
       axios
         .put(`${process.env.REACT_APP_SERVER}/posts/like/${postId}`)
         .then((res) => {
-          localStorage.setItem(`adLiked${postId}`, 'true');
+          localStorage.setItem(`adLiked${user.network_id}${postId}`, 'true');
           getPosts();
           return;
         })
@@ -71,7 +72,7 @@ function Post({
       axios
         .put(`${process.env.REACT_APP_SERVER}/posts/unlike/${postId}`)
         .then((res) => {
-          localStorage.setItem(`adLiked${postId}`, 'false');
+          localStorage.setItem(`adLiked${user.network_id}${postId}`, 'false');
           getPosts();
           return;
         })
@@ -110,187 +111,200 @@ function Post({
 
   return (
     <>
-      {(report < 5 || user.admin) && (
-        <div
-          className='relative p-4 my-3 transition-all duration-150 border border-slate-100 rounded-xl hover:bg-ghostWhite dark:hover:bg-gray-900 dark:hover:border-secondary'
-          style={{ backgroundColor: report > 5 && 'rgba(255, 142, 162, .3)' }}
-        >
-          <div className='flex'>
-            <Avatar
-              alt='avatar'
-              src={
-                post?.user_info?.profile_img ||
-                'https://iptc.org/wp-content/uploads/2018/05/avatar-anonymous-300x300.png'
-              }
-              sx={{ width: 40, height: 40 }}
-              className='mt-1 ml-1 mr-6 ring-2 ring-offset-2 ring-primary'
-            />
-
-            <div className='w-full'>
-              <div className='flex font-medium align-top'>
-                {post.user_info.username}
-              </div>
-              <div className='flex items-center'>
-                <div className='mr-2 text-xs font-light text-slate-500'>
-                  {translateCategory(post.tag)}
-                </div>
-                <> · </>
-                <div className='ml-2 mr-2 text-xs font-light text-slate-500'>
-                  {city && city}
-                </div>
-                <> · </>
-                <div className='ml-2 text-xs font-light text-slate-500'>
-                  {handleTime(time)}
-                </div>
-              </div>
-              <div className='mt-2'>{body}</div>
-              <div className='flex gap-2 py-2'>
-                {photos.map((photo, i) => (
-                  <div key={i}>
-                    <img
-                      onClick={handleModal}
-                      src={photo.image_url}
-                      alt='upload'
-                      width='75px'
-                      className='border border-black'
-                    />
+      {(report < 5 || user.admin || user.network_id === group.admin_id) && (
+        <>
+          {(!post.privacy ||
+            user.admin ||
+            group?.userjoined?.indexOf(user.network_id) !== -1) && (
+            <div
+              className='relative p-4 my-3 transition-all duration-150 border border-slate-100 rounded-xl hover:bg-ghostWhite dark:hover:bg-gray-900 dark:hover:border-secondary'
+              style={{
+                backgroundColor: report > 5 && 'rgba(255, 142, 162, .3)',
+              }}
+            >
+              <div className='flex'>
+                <Avatar
+                  alt='avatar'
+                  src={
+                    post?.user_info?.profile_img ||
+                    'https://iptc.org/wp-content/uploads/2018/05/avatar-anonymous-300x300.png'
+                  }
+                  sx={{ width: 40, height: 40 }}
+                  className='mt-1 ml-1 mr-6 ring-2 ring-offset-2 ring-primary'
+                />
+                <div className='w-full'>
+                  <div className='flex font-medium align-top'>
+                    {post.user_info.username}
+                  </div>
+                  <div className='flex items-center'>
+                    <div className='mr-2 text-xs font-light text-slate-500'>
+                      {translateCategory(post.tag)}
+                    </div>
+                    <> · </>
+                    <div className='ml-2 mr-2 text-xs font-light text-slate-500'>
+                      {city && city}
+                    </div>
+                    <> · </>
+                    <div className='ml-2 text-xs font-light text-slate-500'>
+                      {handleTime(time)}
+                    </div>
+                  </div>
+                  <div className='mt-2'>{body}</div>
+                  <div className='flex gap-2 py-2'>
+                    {photos.map((photo, i) => (
+                      <div key={i}>
+                        <img
+                          onClick={handleModal}
+                          src={photo.image_url}
+                          alt='upload'
+                          width='75px'
+                          className='border border-black'
+                        />
+                        <Modal
+                          open={isEnlarged}
+                          onClose={handleModal}
+                          sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                          }}
+                        >
+                          <Box>
+                            <img
+                              key={i}
+                              onClick={handleModal}
+                              src={photo.image_url}
+                              alt='upload'
+                              width='800px'
+                              className='border border-black'
+                            />
+                          </Box>
+                        </Modal>
+                      </div>
+                    ))}
+                  </div>
+                  <div className='flex items-center justify-between mt-2 mr-2'>
+                    {[
+                      [
+                        'comment',
+                        <MdChatBubbleOutline size='15' />,
+                        handleComment,
+                      ],
+                      [
+                        like,
+                        JSON.parse(localStorage.getItem(`adLiked${postId}`)) ? (
+                          <MdFavorite size='15' color='red' />
+                        ) : (
+                          <MdFavoriteBorder size='15' />
+                        ),
+                        handleLike,
+                      ],
+                      ['share', <MdOutlineShare size='15' />, handleShare],
+                    ].map(([title, icon, handleClick], i) => (
+                      <PostButton
+                        icon={icon}
+                        text={title}
+                        handleClick={handleClick}
+                        key={i}
+                      />
+                    ))}
                     <Modal
-                      open={isEnlarged}
-                      onClose={handleModal}
+                      open={share}
+                      onClose={handleShare}
                       sx={{
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'center',
                       }}
+                      aria-labelledby='modal-title'
+                      aria-describedby='modal-description'
                     >
-                      <Box>
-                        <img
-                          key={i}
-                          onClick={handleModal}
-                          src={photo.image_url}
-                          alt='upload'
-                          width='800px'
-                          className='border border-black'
-                        />
+                      <Box
+                        sx={{
+                          width: 400,
+                          height: 230,
+                          backgroundColor: 'white',
+                          align: 'center',
+                        }}
+                      >
+                        <Typography
+                          variant='h6'
+                          component='h2'
+                          align='center'
+                          sx={{
+                            mt: 2,
+                            mb: 4,
+                          }}
+                        >
+                          Share to...
+                        </Typography>
+                        <Grid
+                          container
+                          align='center'
+                          sx={{
+                            mt: 2,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                          }}
+                        >
+                          <Grid item md={3} xs={5}>
+                            <FacebookShareButton
+                              url='http://facebook.com'
+                              // url={window.location.href}
+                              hashtag={'#hashtag'}
+                              quote={`New post from ${post.user_info.username}: ${post.body}. Please check the latest posts at ${window.location.href}`}
+                            >
+                              <FacebookIcon size={32} round />
+                            </FacebookShareButton>
+                            <div>Facebook</div>
+                          </Grid>
+                          <Grid item md={3} xs={5}>
+                            <TwitterShareButton
+                              url={window.location.href}
+                              hashtag={'#hashtag'}
+                              title={`New post from ${post.user_info.username}: ${post.body}. Please check the latest posts at`}
+                            >
+                              <TwitterIcon size={32} round />
+                            </TwitterShareButton>
+                            <div>Twitter</div>
+                          </Grid>
+                          <Grid item md={3} xs={5}>
+                            <PinterestShareButton
+                              url={window.location.href}
+                              hashtag={'#hashtag'}
+                              media='https://picsum.photos/200/300'
+                              description={`New post from ${post.user_info.username}: ${post.body}.`}
+                            >
+                              <PinterestIcon size={32} round />
+                            </PinterestShareButton>
+                            <div>Pinterest</div>
+                          </Grid>
+                          <Grid item md={3} xs={5}>
+                            <LineShareButton
+                              url={window.location.href}
+                              hashtag={'#hashtag'}
+                              title={`New post from ${post.user_info.username}: ${post.body}.`}
+                            >
+                              <LineIcon size={32} round />
+                            </LineShareButton>
+                            <div>Line</div>
+                          </Grid>
+                        </Grid>
                       </Box>
                     </Modal>
                   </div>
-                ))}
+                </div>
               </div>
-              <div className='flex items-center justify-between mt-2 mr-2'>
-                {[
-                  ['comment', <MdChatBubbleOutline size='15' />, handleComment],
-                  [
-                    like,
-                    JSON.parse(localStorage.getItem(`adLiked${postId}`)) ? (
-                      <MdFavorite size='15' color='red' />
-                    ) : (
-                      <MdFavoriteBorder size='15' />
-                    ),
-                    handleLike,
-                  ],
-                  ['share', <MdOutlineShare size='15' />, handleShare],
-                ].map(([title, icon, handleClick], i) => (
-                  <PostButton
-                    icon={icon}
-                    text={title}
-                    handleClick={handleClick}
-                    key={i}
-                  />
-                ))}
-                <Modal
-                  open={share}
-                  onClose={handleShare}
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                  aria-labelledby='modal-title'
-                  aria-describedby='modal-description'
-                >
-                  <Box
-                    sx={{
-                      width: 400,
-                      height: 230,
-                      backgroundColor: 'white',
-                      align: 'center',
-                    }}
-                  >
-                    <Typography
-                      variant='h6'
-                      component='h2'
-                      align='center'
-                      sx={{
-                        mt: 2,
-                        mb: 4,
-                      }}
-                    >
-                      Share to...
-                    </Typography>
-                    <Grid
-                      container
-                      align='center'
-                      sx={{
-                        mt: 2,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }}
-                    >
-                      <Grid item md={3} xs={5}>
-                        <FacebookShareButton
-                          url={window.location.href}
-                          hashtag={'#hashtag'}
-                          quote={`New post from ${post.user_info.username}: ${post.body}.`}
-                        >
-                          <FacebookIcon size={32} round />
-                        </FacebookShareButton>
-                        <div>Facebook</div>
-                      </Grid>
-                      <Grid item md={3} xs={5}>
-                        <TwitterShareButton
-                          url={window.location.href}
-                          hashtag={'#hashtag'}
-                          title={`New post from ${post.user_info.username}: ${post.body}. Please check the latest update at`}
-                        >
-                          <TwitterIcon size={32} round />
-                        </TwitterShareButton>
-                        <div>Twitter</div>
-                      </Grid>
-                      <Grid item md={3} xs={5}>
-                        <PinterestShareButton
-                          url={window.location.href}
-                          hashtag={'#hashtag'}
-                          media='https://picsum.photos/200/300'
-                          description={`New post from ${post.user_info.username}: ${post.body}.`}
-                        >
-                          <PinterestIcon size={32} round />
-                        </PinterestShareButton>
-                        <div>Pinterest</div>
-                      </Grid>
-                      <Grid item md={3} xs={5}>
-                        <LineShareButton
-                          url={window.location.href}
-                          hashtag={'#hashtag'}
-                          title={`New post from ${post.user_info.username}: ${post.body}.`}
-                        >
-                          <LineIcon size={32} round />
-                        </LineShareButton>
-                        <div>Line</div>
-                      </Grid>
-                    </Grid>
-                  </Box>
-                </Modal>
-              </div>
+              <MoreMenu
+                postId={postId}
+                getPosts={getPosts}
+                user={user}
+                post={post}
+                group={group}
+              />
             </div>
-          </div>
-          <MoreMenu
-            postId={postId}
-            getPosts={getPosts}
-            user={user}
-            post={post}
-          />
-        </div>
+          )}
+        </>
       )}
     </>
   );
