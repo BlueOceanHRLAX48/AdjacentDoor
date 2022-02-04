24b2d6402df8576c32a3c9dbb593e42439d0d1f2
Ernest Zhang <ernestzhang97@gmail.com>
2022-02-03 19:21:16 -0800

    switched config

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 require ('dotenv').config()
 
-// module.exports = {
-//   username: process.env.PG_USER,
-//   database: process.env.PG_DATABASE,
-//   host: process.env.PG_HOST,
-//   password: process.env.PG_PASSWORD,
-//   port: process.env.PG_PORT
-// }
+module.exports = {
+  username: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
+  host: process.env.PG_HOST,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT
+}
 
 // local testing
-module.exports= {
-  username: 'ernestzhang',
-  database: 'blueocean',
-  host: 'localhost',
-  password: '',
-  port: 5432
-}
+// module.exports= {
+//   username: 'ernestzhang',
+//   database: 'blueocean',
+//   host: 'localhost',
+//   password: '',
+//   port: 5432
+// }
