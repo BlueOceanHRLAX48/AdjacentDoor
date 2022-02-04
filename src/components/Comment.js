25ea97817ad2b244afaadb19f99caf15ad189f0c
milesmaroney <miles@milesmaroney.com>
2022-02-04 10:35:05 -0800

    Fixed comments showing up in the correct post and fixed timestamps using local timezone

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,6 +2,15 @@ import { Avatar } from '@mui/material';
 import moment from 'moment';
 
 function Comment({ comment }) {
+  function handleTime(timestamp) {
+    timestamp = timestamp + 'Z';
+    return moment().isSame(timestamp, 'day')
+      ? moment(timestamp).fromNow()
+      : moment(timestamp).format('LL');
+  }
+
+  console.log(comment);
+
   return (
     <div className='flex pt-4 border-t'>
       <Avatar
@@ -15,12 +24,14 @@ function Comment({ comment }) {
       />
       <div className='w-full'>
         <div className='flex items-center'>
-          <div className='flex mr-2 font-medium text-xs align-top'>{comment?.username}</div>
+          <div className='flex mr-2 font-medium text-xs align-top'>
+            {comment?.username}
+          </div>
 
           <> · </>
 
           <div className='ml-2 text-xs font-light text-slate-500'>
-            {moment(comment?.time).fromNow()}
+            {handleTime(comment?.time)}
           </div>
         </div>
         <div className='mt-2'>{comment?.body}</div>
